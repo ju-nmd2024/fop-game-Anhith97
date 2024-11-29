e816b9799f59849dbe39d803f35e2b6e60d12d5f
Anhith Dinesh Polotharayil <dian24pr@student.ju.se>
2024-11-29 01:12:54 +0100

    Create skier.js

---
+++ skier.js
@@ -0,0 +1,47 @@
+//The character
+strokeWeight(1);
+background(255, 255, 255);
+//The hat
+fill(255, 255, 255);
+triangle(435, 440, 450, 415, 465, 440);
+fill(255, 0, 0);
+ellipse(450, 450, 35);
+ellipse(450, 418, 10);
+//The Body
+fill(255, 255, 255);
+rect(436, 475, 30, 35, 5);
+rect(462, 475, 10, 15, 5);
+rect(429, 475, 10, 15, 5);
+//Hand
+fill(0, 0, 0);
+ellipse(434, 507, 10, 12);
+ellipse(467, 507, 10, 12);
+fill(255, 200, 150);
+rect(429, 487, 10, 17, 1);
+rect(462, 487, 10, 17, 1);
+//The Face
+fill(255, 200, 150);
+ellipse(433, 460, 5, 10);
+ellipse(467, 460, 5, 10);
+ellipse(450, 460, 33);
+fill(0, 0, 0);
+arc(450, 465, 8, 5, 0, 3.14);
+//The Eyes
+fill(255, 255, 255);
+ellipse(445, 458, 5, 2);
+ellipse(455, 458, 5, 2);
+fill(0, 0, 0);
+ellipse(445, 458, 1);
+ellipse(455, 458, 1);
+//The Shoes
+fill(0, 0, 0);
+ellipse(444, 540, 15, 20);
+ellipse(458, 540, 15, 20);
+//jeans
+fill(255, 0, 0);
+rect(436, 505, 15, 35, 1);
+rect(451, 505, 15, 35, 1);
+//The skate
+fill(255, 255, 255);
+rect(439, 547, 10, 27, 3);
+rect(453, 547, 10, 27, 3);
